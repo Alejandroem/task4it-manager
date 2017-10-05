c6436724659299c6981b32f9e24013fa15c23389
alejandroem <alexalejandroem@gmail.com>
2017-10-04 21:04:42 -0600

    Added database structure models and controllers 	new file:   app/File.php 	new file:   app/Http/Controllers/FileController.php 	new file:   app/Http/Controllers/ProjectController.php 	new file:   app/Http/Controllers/QuestionController.php 	new file:   app/Http/Controllers/RequirementController.php 	new file:   app/Project.php 	modified:   app/Providers/AppServiceProvider.php 	new file:   app/Question.php 	new file:   app/Requirement.php 	new file:   database/migrations/2017_10_05_023116_create_projects_table.php 	new file:   database/migrations/2017_10_05_023147_create_requirements_table.php 	new file:   database/migrations/2017_10_05_023156_create_questions_table.php 	new file:   database/migrations/2017_10_05_023236_create_files_table.php 	new file:   database/migrations/2017_10_05_024944_create_project_user_table.php

---
+++ database/migrations/2017_10_05_023156_create_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('requirement_id')->unsigned();
+            $table->foreign('requirement_id')->references('id')->on('requirements')->onDelete('cascade');
+            $table->integer('question_id')->unsigned()->nullable();
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');;
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
