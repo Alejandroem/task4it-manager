e6b1cd61ff5d44746b796bd05bfe73b92d4e6bea
Alejandro Enríquez <alexalejandroem@gmail.com>
2017-10-19 11:05:07 -0600

    Added role client on show user controller  Changes to be committed: 	modified:   app/Http/Controllers/UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 
     public function __construct()
     {
-        $this->middleware(['role:admin|project-manager']);        
+        $this->middleware(['role:admin|project-manager|client']);        
         $this->middleware('auth');
     }
     /**
